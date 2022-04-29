6bd3209b17d597f4879773b754fa8a68d366f51c
Xavierfroster <39325953+Xavierfroster@users.noreply.github.com>
2022-04-29 16:55:53 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello{
 public static void main(String[] args) {
 	for(int i=0;i<4;i++){
 		System.out.println("Hello, World!");
-		System.out.println("Java World")
+		System.out.println("Java World");
 	}
 }
 }
